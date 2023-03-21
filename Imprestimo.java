94b04daf94fd353c63cd5239b5d13d7875210378
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2023-03-21 10:44:25 -0300

    Create Imprestimo.java

---
+++ Imprestimo.java
@@ -0,0 +1,16 @@
+public class Salario {
+
+   public static void main(String[] args) {
+      Scanner salario = new Scanner(System.in);
+      System.out.println("Digite o seu salario:");
+      int num = salario.nextInt();
+      if (num >= 5*num) {
+         System.out.println("OK");
+      }
+      else {
+         System.out.println("WTF AMIGO VOCE ESTARE LOUCO?");
+      }
+
+   }
+
+}
