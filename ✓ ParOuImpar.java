be3bef006b80cfc417ba820c62b330327a8657ae
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2023-03-21 10:39:26 -0300

    Update ✓ ParOuImpar.java

--- ✓ ParOuImpar.java
+++ ✓ ParOuImpar.java
@@ -9,8 +9,8 @@ public class Catch_User {
    public static void main(String[] args) {
       Scanner teclado = new Scanner(System.in); 
       System.out.println("Digite um munero:");
-      int numero = teclado.nextInt();
-      if (numero >= 0) {
+      int num = teclado.nextInt();
+      if (num>= 0) {
          int ehPar = num % 2;
          if (ehPar == 0) {
             System.out.println("O numero é par.");
